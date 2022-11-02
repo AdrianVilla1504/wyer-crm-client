31323a8b962ac0c90a534369d8d7594e184d5e34
adrianvilla1504 <adriancvilla@gmail.com>
2022-11-02 16:50:32 -0500

    feat: style contactListSection

--- src/components/ContactHomeCard/index.jsx
+++ src/components/ContactHomeCard/index.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const ContactHomeCard = ({ client }) => {
   return (
-      <>
-        <li className="flex flex-row" key={client._id}>
-          <div className="select-none w-[90vw] lg:w-[80vw] cursor-pointer flex flex-row justify-between items-center px-[25px] py-[17px]">
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <li className="flex flex-row " key={client._id}>
+          <div className="select-none w-[90vw] lg:w-[80vw] cursor-pointer flex flex-row justify-between items-center px-[8vw] lg:px-[3vw] py-[1vw]">
             <div className="flex flex-col w-[50vw] ">
               <div className="w-full font-medium text-[#242424]">
-                {client.name}
+                {client.name + " " + client.lastName}
               </div>
-              <div className="text-[#186F9A] text-sm">
-                (+57) {client.phone}
-              </div>
+              <div className="text-[#186F9A] text-sm">(+57) {client.phone}</div>
             </div>
             <div className="flex flex-row justify-end">
               <button className="w-[30px] ml-[0px] text-right flex justify-end">
@@ -38,8 +37,9 @@ const ContactHomeCard = ({ client }) => {
             </div>
           </div>
         </li>
-      </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
 export default ContactHomeCard;
--- src/components/ContactListSection/index.jsx
+++ src/components/ContactListSection/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 import React from "react";
 import { useState, useEffect } from "react";
 import ContactHomeCard from "../ContactHomeCard/";
@@ -5,8 +6,6 @@ import { getClients } from "../../services/clients";
 
 const ContacListSection = () => {
   const [clients, setClient] = useState();
-
-  console.log(clients);
 
   useEffect(() => {
     const fetchClient = async () => {
@@ -18,14 +17,13 @@ const ContacListSection = () => {
   return (
     <>
       {clients ? (
-
-          <div className="w-[90vw] lg:w-[80vw] px-[40px] container flex flex-col mx-auto items-center justify-center bg-[#F6F6F6] rounded-[33px]">
-            <ul className="flex flex-col divide divide-y">
-              {clients?.map((client) => {
-                return <ContactHomeCard client={client} key={client._id} />;
-              })}
-            </ul>
-          </div>
+        <div className="w-[90vw] lg:w-[80vw] py-[20px] container flex flex-col mx-auto items-center justify-center bg-[#F6F6F6] rounded-[33px]">
+          <ul className="flex w-[74vw] lg:w-[74vw] flex-col divide divide-y-[3px] divide-white">
+            {clients?.map((client) => {
+              return <ContactHomeCard client={client} key={client._id} />;
+            })}
+          </ul>
+        </div>
       ) : (
         <div className="w-[90vw] h-[90vh] lg:w-[80vw] container flex flex-col mx-auto items-center justify-center bg-F6F6F6 rounded-lg ">
           <img
