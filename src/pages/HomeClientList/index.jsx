c03ed78416a0fc668be2d2f1c3a47c77a8396578
adrianvilla1504 <adriancvilla@gmail.com>
2022-11-02 21:06:33 -0500

    feat: add search input design to client list view

--- src/pages/HomeClientList/index.jsx
+++ src/pages/HomeClientList/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import InputSearchClient from "../../components/InputSearchClient";
 import ContacListSection from "../../components/ContactListSection";
 import ContactFooter from "../../components/ContactFooter";
 
 const HomeClientList = () => {
   return (
-    <div className="flex flex-col justify-between">
-      <div className= "h-[100vh]">
+    <div className="flex flex-col justify-start">
+      <div className="fixed flex justify-center w-[100vw] pb-[20px]">
+        <InputSearchClient />
+      </div>
+      <div className= "pt-[20vh] h-[100vh] lg:pt-[26vh] lg:mb-[26vh]">
         <ContacListSection />
       </div>
       <div className="fixed bottom-0 w-[100vw]">
