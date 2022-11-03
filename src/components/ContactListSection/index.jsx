4795967f43be27b94d69d3c4c10fc60c75c9f8c4
adrianvilla1504 <adriancvilla@gmail.com>
2022-11-03 00:32:05 -0500

    feat: init add client view, design add button

--- src/components/ContactListSection/index.jsx
+++ src/components/ContactListSection/index.jsx
@@ -2,12 +2,11 @@
 import React from "react";
 import ContactHomeCard from "../ContactHomeCard/";
 
-const ContacListSection = ({clients}) => {
-
+const ContacListSection = ({ clients }) => {
   return (
     <>
       {clients ? (
-        <div className="pb-[16vh] lg:pb-[20vh]">
+        <div className="flex flex-col justify-center items-center pb-[16vh] lg:pb-[20vh]">
           <div className="w-[90vw] lg:w-[80vw] py-[20px] lg:mt-[4vh] container flex flex-col mx-auto items-center justify-center bg-[#F6F6F6] rounded-[33px]">
             <ul className="flex w-[74vw] lg:w-[74vw] flex-col divide divide-y-[3px] divide-white">
               {clients?.map((client) => {
@@ -15,6 +14,15 @@ const ContacListSection = ({clients}) => {
               })}
             </ul>
           </div>
+          <button className="flex flex-row justify-between items-center w-[37vw] h-[40px] my-[15px] bg-[#006191] hover:bg-blue-700  py-2 px-4 rounded-full">
+            <span className="text-white text-lg font-medium">Agregar</span>
+
+            <img
+                  className="w-[22px]"
+                  src="https://res.cloudinary.com/dkagy4g5m/image/upload/v1667452901/wyer/add-contact_bgxq7q.png"
+                  alt="add_contact"
+                />
+          </button>
         </div>
       ) : (
         <div className="w-[90vw] lg:w-[80vw] h-[100vh] container flex flex-col mx-auto items-center justify-center bg-F6F6F6 rounded-lg ">
