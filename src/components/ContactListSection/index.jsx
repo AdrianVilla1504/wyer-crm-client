3868e8cfc07cd6415c41da54e3059c38aebf675f
adrianvilla1504 <adriancvilla@gmail.com>
2022-11-04 19:48:10 -0500

    feat: add fix button to bottom, and post patch issues on forms

--- src/components/ContactListSection/index.jsx
+++ src/components/ContactListSection/index.jsx
@@ -1,15 +1,13 @@
 /* eslint-disable no-unused-expressions */
 import React from "react";
 
-import { Link } from "react-router-dom";
-
 import ContactHomeCard from "../ContactHomeCard/";
 
 const ContacListSection = ({ contacts }) => {
   return (
     <>
       {contacts ? (
-        <div className="flex flex-col justify-center items-center pb-[16vh] lg:pb-[20vh]">
+        <div className="flex flex-col justify-center items-center pb-[25vh] lg:pb-[20vh]">
           <div className="w-[90vw] lg:w-[80vw] py-[20px] lg:mt-[4vh] container flex flex-col mx-auto items-center justify-center bg-[#F6F6F6] rounded-[33px]">
             <ul className="flex w-[74vw] lg:w-[74vw] flex-col divide divide-y-[3px] divide-white">
               {contacts?.map((contact) => {
@@ -17,17 +15,6 @@ const ContacListSection = ({ contacts }) => {
               })}
             </ul>
           </div>
-          <Link to="/CreateNewContact">
-            <button className="flex flex-row justify-between items-center w-[37vw] h-[40px] my-[15px] bg-[#006191] hover:bg-blue-700  py-2 px-4 rounded-full">
-              <span className="text-white text-lg font-medium">Agregar</span>
-
-              <img
-                className="w-[22px]"
-                src="https://res.cloudinary.com/dkagy4g5m/image/upload/v1667452901/wyer/add-contact_bgxq7q.png"
-                alt="add_contact"
-              />
-            </button>
-          </Link>
         </div>
       ) : (
         <div className="w-[90vw] lg:w-[80vw] h-[100vh] container flex flex-col mx-auto items-center justify-center bg-F6F6F6 rounded-lg ">
