a606e7834b4e652465938458f308f3ec8ceb766e
adrianvilla1504 <adriancvilla@gmail.com>
2022-11-25 14:39:01 -0500

    feat: Fix loading icon on contact list section

--- src/components/ContactListSection/index.jsx
+++ src/components/ContactListSection/index.jsx
@@ -16,7 +16,7 @@ const ContactListSection = ({ contacts }) => {
           </div>
         </div>
       ) : (
-        <div className="w-[90vw] lg:w-[80vw] h-[100vh] container flex flex-col mx-auto items-center justify-center bg-F6F6F6 rounded-lg ">
+        <div className="w-[90vw] lg:w-[80vw] h-[100vh] h-[60vh] container flex flex-col mx-auto items-center justify-center bg-F6F6F6 rounded-lg ">
           <img
             className="animate-bounce w-[30px]"
             src="https://res.cloudinary.com/dkagy4g5m/image/upload/v1667411126/wyer/contact_loader_w1vmxz.png"
